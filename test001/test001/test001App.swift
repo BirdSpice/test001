91c3be21d5f32ca022e4d6e63172054820992dfa
BirdSpice <goodday200580@gmail.com>
2024-03-22 03:30:03 +0900

    add files

---
+++ test001/test001/test001App.swift
@@ -0,0 +1,17 @@
+//
+//  test001App.swift
+//  test001
+//
+//  Created by user on 2024/03/20.
+//
+
+import SwiftUI
+
+@main
+struct test001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
